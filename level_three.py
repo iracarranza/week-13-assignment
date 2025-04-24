9adcb5184c357c422512ef5d11d55c6f8b3f0e9f
Ira Carranza <iracarranza@Iras-MacBook-Pro-2.local>
2025-04-24 09:45:06 -0400

    level 3 complete, changed to use no built in methods

--- level_three.py
+++ level_three.py
@@ -1,6 +1,20 @@
+# i forgot it was no methods
+
 def findMaxInList(numList):
     numList.sort(reverse = True)
     return numList[0]
 
 exampleList = [1, 1.1, 1.2, 1.3, 14, 1.5]
 print(findMaxInList(exampleList))
+
+###
+
+def findMaxInList2(numbers):
+    maxValue = numbers[0]
+    for eachNum in numbers:
+        if eachNum > maxValue:
+            maxValue = eachNum
+    return maxValue
+
+exList2 = [-10, -11, 59, -60, 100]
+print (findMaxInList2(exList2))
