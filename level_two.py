a7195fdbe8b511624d39cca30ac042474d21fec6
Ira Carranza <iracarranza@Iras-MacBook-Pro-2.local>
2025-04-24 08:25:58 -0400

    Level 2 Complete

---
+++ level_two.py
@@ -0,0 +1,6 @@
+mix_of_stuff = ["string", 10, 9.9, True]
+
+playlist_for_coding = ["the judge", "next semester", "this side of paradise", "constellations", "lily magnolia"]
+print(playlist_for_coding[3])
+
+playlist_for_coding.append("abracadabra")
